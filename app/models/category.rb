283a1085d18c25c9f8043630bfe744558f1ff45a
Kaio Cesar <tecnico.kaio@gmail.com>
2020-04-15 16:37:19 -0300

    endpoint de show de categorias exibindo artistas e musicas das categorias

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,14 @@ class Category < ApplicationRecord
     validates :name, presence: true
 
     has_one_attached :image
+
+    def artists
+        # esse metodo retornará artistas da categoria
+        Artist.joins(:albums).where(albums: { id: self.albums.ids }).distinct
+    end
+
+    def songs
+        # esse metodo retornará as músicas da categoria
+        Song.joins(:album).where(songs: { id: self.albums.ids }).distinct
+    end
 end
